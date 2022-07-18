e8c84f465ad670e9863d45b0ded0c366614f89b5
Fabio <akki.fabio@gmail.com>
2022-07-18 19:34:46 -0300

    Added features

--- CSHARP_Full_Calculator/Full_Calculator/Program.cs
+++ CSHARP_Full_Calculator/Full_Calculator/Program.cs
@@ -6,43 +6,55 @@ namespace Full_Calculator
     {
         static void Main(string[] args)
         {
-            string desligar = "0";
-
-            Console.WriteLine("Isto é uma Calculadora.");
-            Console.WriteLine("Digite '1' para continuar ou '0' para finalizar o programa");
-            string ligar = Console.ReadLine();
-            Console.WriteLine("Digite o primeiro número: ");
-            if (ligar == "1")
-            {
-                while (desligar != "0")
-
-                    Console.WriteLine("Digite o primeiro número: ");
-                    double num1 = double.Parse(Console.ReadLine());
-
-                    Console.WriteLine("Digite o operador (+) para Adição, (-) Subtração, (*) Multiplicação ou (/) Divisão: ");
-                    string operador = Console.ReadLine();
-
-                    Console.WriteLine("Digite o segundo número: ");
-                    double num2 = double.Parse(Console.ReadLine());
+            int ligar = 1;
+                while (ligar == 1)
+            {  
+                Console.WriteLine("------------------------");
+                Console.WriteLine("Esta é a calculadora da Mallu!");
+                Console.WriteLine("Digite '1' para continuar ou '0' para finalizar o programa");
+                Console.WriteLine("------------------------");
+                ligar = int.Parse(Console.ReadLine());
+                if (ligar != 1)
+            
+                {
+                    Console.WriteLine("Desligando");
+                    break;
+                }
                 
 
-                switch (operador)
                 {
-                    case "+":
-                        Console.WriteLine("Resultado da soma: " + somar(num1, num2));
-                        break;
-                    case "-":
-                        Console.WriteLine("Resultado da subtração: " + subtrair(num1, num2));
-                        break;
-                    case "*":
-                        Console.WriteLine("Resultado da multiplicação: " + multiplicar(num1, num2));
-                        break;
-                    case "/":
-                        Console.WriteLine("Resultado da divisão: " + dividir(num1, num2));
-                        break;
+
+                    {
+                        Console.WriteLine("Digite o primeiro número: ");
+                        double num1 = double.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Digite o operador (+) para Adição, (-) Subtração, (*) Multiplicação ou (/) Divisão: ");
+                        string operador = Console.ReadLine();
+
+                        Console.WriteLine("Digite o segundo número: ");
+                        double num2 = double.Parse(Console.ReadLine());
+
+
+                        switch (operador)
+                        {
+                            case "+":
+                                Console.WriteLine($"Resultado da soma: {num1} + {num2} = " + somar(num1, num2));
+                                break;
+                            case "-":
+                                Console.WriteLine($"Resultado da subtração: {num1} - {num2} = " + subtrair(num1, num2));
+                                break;
+                            case "*":
+                                Console.WriteLine($"Resultado da multiplicação: {num1} * {num2} = " + multiplicar(num1, num2));
+                                break;
+                            case "/":
+                                Console.WriteLine($"Resultado da divisão: {num1} / {num2} = " + dividir(num1, num2));
+                                break;
+                        }
+                    }
+                
                 }
-            
             }
+            
         }
 
         public static string somar(double num1, double num2)
